99c6c15a9994de446c5486921b8a955a5b3a414f
shield-solution <shieldsolutiongroup@gmail.com>
2025-02-20 12:51:35 +0300

    fix: Update footer link to Instagram
    - Changed the footer link from the homepage to the official Instagram page of Shield Sg7.
    - The link now opens in a new tab for better user experience.
    
    This update enhances social media engagement by providing direct access to the organization's Instagram profile.

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -116,7 +116,10 @@ const Footer = () => {
                   />
                 </svg>
               </Link>
-              <Link href="/">
+              <Link
+                href="https://www.instagram.com/shieldssg7/"
+                target="__blank"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="28"
